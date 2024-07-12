251db5f57dedda0a68d70f74e26ea2dbb691b6c5
sfm46 <137821491+sfm46@users.noreply.github.com>
2024-07-12 19:42:21 +0530

    Add files via upload

---
+++ ehrservice/src/test/java/com/healthcare/ehrservice/EhrServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthcare.ehrservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EhrServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
